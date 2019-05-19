6bcc55806c773c236dc216a4280d46c563e95e40
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2019-05-19 12:59:06 +0200

    Add the cons, basis for all fn invocations.

---
+++ lib/feeble/runtime/list.rb
@@ -0,0 +1,82 @@
+module Feeble::Runtime
+  class List
+    class EmptyList
+      def first
+        List::EMPTY
+      end
+
+      def rest
+        List::EMPTY
+      end
+
+      def cons(obj)
+        List.create obj
+      end
+
+      def conj(obj)
+        cons obj
+      end
+
+      def apnd(obj)
+        cons obj
+      end
+
+      def count
+        0
+      end
+    end
+
+    EMPTY = EmptyList.new
+
+    attr_reader :first, :rest, :count
+
+    def self.create(*args)
+      return EMPTY if args.length == 0
+      new args[0], create(*args[1..args.length]), count: args.length
+    end
+
+    def initialize(obj, rest = EMPTY, count: 1)
+      @count = count
+      @first = obj
+      @rest = rest
+    end
+
+    def cons(obj)
+      self.class.new obj, self, count: count + 1
+    end
+
+    def conj(*args)
+      args.reduce(self) { |list, obj|
+        self.class.new obj, list, count: list.count + 1
+      }
+    end
+
+    def apnd(*args)
+      args.reduce(self) { |list, obj|
+        self.class.new list.first, list.rest.apnd(obj), count: list.count + 1
+      }
+    end
+
+    def each(&block)
+      if block_given?
+        block.call first
+        rest.each(&block) if rest != EMPTY
+      else
+        to_enum(:each)
+      end
+    end
+
+    def ==(other)
+      return false if self.class != other.class
+      first == other.first && rest == other.rest
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def hash
+      @first.hash + @second.hash + :fbl_list.hash
+    end
+  end
+end
---
+++ spec/feeble/runtime/list_spec.rb
@@ -0,0 +1,93 @@
+module Feeble::Runtime
+  RSpec.describe List do
+    subject(:list) { described_class }
+
+    context "constructor function" do
+      it "creates a list with one element" do
+        new_list = list.create 1
+
+        expect(new_list).to eq list.new 1
+        expect(new_list.count).to eq 1
+      end
+
+      it "creates a list with two elements" do
+        new_list = list.create 1, 2
+
+        expect(new_list).to eq list.new 1, list.new(2)
+        expect(new_list.count).to eq 2
+      end
+
+      it "creates a list with three elements" do
+        new_list = list.create 1, 2, 3
+
+        expect(new_list).to eq list.new 1, list.new(2, list.new(3))
+        expect(new_list.count).to eq 3
+      end
+    end
+
+    describe "#rest" do
+      it "returns a list with all but the first element" do
+        new_list = list.create 1, 2, 3, 4
+
+        expect(new_list.rest).to eq list.create 2, 3, 4
+      end
+
+      it "returns an empty list if just one element exists" do
+        new_list = list.create 1
+
+        expect(new_list.rest).to eq list::EMPTY
+      end
+    end
+
+    describe "#cons" do
+      it "prepends an object" do
+        new_list = list.create(2, 3).cons 1
+
+        expect(new_list).to eq list.create 1, 2, 3
+        expect(new_list.count).to eq 3
+      end
+
+      it "returns a new_list with just one element when cons(ing) to Empty List" do
+        new_list = list::EMPTY.cons 1
+
+        expect(new_list).to eq list.new 1
+        expect(new_list.count).to eq 1
+      end
+    end
+
+    describe "#conj" do
+      it "prepends arguments to the new_list" do
+        new_list = list.create(1, 2, 3, 4).conj 5, 6, 7
+
+        expect(new_list).to eq list.create 7, 6, 5, 1, 2, 3, 4
+        expect(new_list.count).to eq 7
+      end
+
+      it "returns a new_list of one when conj(ing) empty new_list" do
+        new_list = list::EMPTY.conj 1
+
+        expect(new_list).to eq list.new 1
+        expect(new_list.count).to eq 1
+      end
+    end
+
+    describe "#apnd" do
+      it "append arguments to the new_list" do
+        new_list = list.create(1, 2).apnd 3, 4, 5
+
+        expect(new_list).to eq list.create 1, 2, 3, 4, 5
+        expect(new_list.count).to eq 5
+      end
+    end
+
+    context "Ruby" do
+      describe "#to_enum" do
+        it "transforms the list into an Enumerator" do
+          new_list = list.create 1, 2, 3, 4, 5, 6, 7
+
+          expect(new_list.to_enum.to_a).to eq [1, 2, 3, 4, 5, 6, 7]
+        end
+      end
+    end
+  end
+end
