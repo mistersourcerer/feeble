1eb636b6d7e81094b26445bd5b60b2ed4dd522fb
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2019-05-26 16:18:33 +0200

    Make it possible to wrap an env with another.
    This allows scoping, if something is not found in
    the actual env, we keep searching in the external
    one.
    
    Now WE CAN HAZ closures.

--- lib/feeble/runtime/env.rb
+++ lib/feeble/runtime/env.rb
@@ -2,8 +2,8 @@ module Feeble::Runtime
   class Env
     # Could be replace for a decent map implementation somewhen
     class Map
-      def initialize
-        @store = {}
+      def initialize(store = {})
+        @store = store
       end
 
       def put(symbol, value)
@@ -13,10 +13,22 @@ module Feeble::Runtime
       def get(symbol)
         @store[symbol]
       end
+
+      def merge(map)
+        self.class.new map.store.merge(@store)
+      end
+
+      protected
+
+      def store
+        @store
+      end
     end
 
-    def initialize
-      @registry = Map.new
+    def initialize(registry = Map.new)
+      @registry = registry
+      @verify = Verifier.new
+      @envs = []
     end
 
     def lookup(id)
@@ -25,6 +37,28 @@ module Feeble::Runtime
 
     def register(id, value = nil)
       @registry.put id, value
+    end
+
+    def wrap(env)
+      raise "Expected #{env} to be an Env" unless @verify.env? env
+
+      self.class.new env.registry.merge(@registry)
+    end
+
+    def invoke(*params)
+      apply_1 params.first if params.length == 1
+    end
+
+    protected
+
+    def registry
+      @registry
+    end
+
+    private
+
+    def apply_1(param)
+      return lookup param if @verify.symbol? param
     end
   end
 end
--- spec/feeble/runtime/env_spec.rb
+++ spec/feeble/runtime/env_spec.rb
@@ -5,6 +5,7 @@ module Feeble::Runtime
     context "#register and #lookup" do
       it "associates a key with an object" do
         env.register Symbol.new(:omg), "lol"
+
         expect(env.lookup(Symbol.new(:omg))).to eq "lol"
       end
     end
@@ -12,8 +13,23 @@ module Feeble::Runtime
     describe "#lookup" do
       it "fetches the value associated with a symbol" do
         env.register Symbol.new(1), "omg"
+
         expect(env.lookup(Symbol.new(1))).to eq "omg"
       end
+
+      context "scoping environments" do
+        it "searches for values in the 'internal' environments first" do
+          env.register Symbol.new(:override), "omg"
+          internal = described_class.new.tap { |e|
+            e.register Symbol.new(:override), "lol"
+          }
+          new_env = env.wrap internal
+
+          expect(new_env.lookup(Symbol.new(:override))).to eq "lol"
+        end
+      end
+    end
+
     end
   end
 end
