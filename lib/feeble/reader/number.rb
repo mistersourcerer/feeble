af1ef8d6f069147b386719586cf3ab73bed36f5f
Ricardo Valeriano <mister.sourcerer@gmail.com>
2019-07-20 19:19:17 +0200

    Introduce a reader specialised in Numbers.

---
+++ lib/feeble/reader/number.rb
@@ -0,0 +1,40 @@
+module Feeble::Reader
+  class Number
+    START = /\A[\d\-_]{1}/
+    BODY = /\A[\d\-_\.]{1}/
+
+    include Feeble::Runtime
+    include Invokable
+
+    def initialize
+      prop :reader
+
+      arity(Symbol.new(:code)) { |env|
+        source_code = env.lookup(Symbol.new(:code))
+        number, code = read_number_from source_code
+        [parse_number(number), code]
+      }
+    end
+
+    private
+
+    FLOAT = /\A[\-\d_]+\.[\d_]+\z/
+
+    def read_number_from(source_code, current_number = Str.create(""))
+      finished_or_not_number = (source_code.empty? ||
+        (current_number.empty? && !START.match?(source_code.first)))
+      return current_number, source_code if finished_or_not_number
+      return current_number, source_code if !BODY.match?(source_code.first)
+
+      read_number_from(
+        source_code.rest, current_number.apnd(source_code.first))
+    end
+
+    def parse_number(number)
+      number = String(number)
+      return nil if number.length == 0
+
+      FLOAT.match?(number) ? Float(number) : Integer(number)
+    end
+  end
+end
--- lib/feeble/reader/read.rb
+++ lib/feeble/reader/read.rb
@@ -4,12 +4,73 @@ module Feeble::Reader
     include Invokable
 
     def initialize
-      arity(Symbol.new(:code)) { |env| read env.lookup(Symbol.new(:code)) }
+      @fn = ListFunctions.new(StrEmpty.instance)
+      @number = Number.new
+
+      arity(Symbol.new(:code)) { |env|
+        read Str.create(env.lookup(Symbol.new(:code))), env
+      }
     end
 
     private
 
-    def read(string)
+    SEPARATOR = /\A[\s,]/
+
+    attr_reader :fn
+
+    def read(string, env, token: Str.create(""), code: [])
+      if fn.empty? string
+        if !token.empty?
+          atom, _ = read_token(string, env, token: token, code: code)
+          code << atom
+        end
+
+        return code
+      end
+
+      new_string, new_token = *
+        case String(string.first)
+        when SEPARATOR
+          read_separator string, env, token: token, code: code
+        when Number::START
+          read_number string, env, code: code
+        else
+          [string.rest, token.apnd(string.first)]
+        end
+
+      read(new_string, env, token: new_token, code: code)
+    end
+
+    def read_separator(string, env, token: Str.create(""), code: [])
+      return [string, token] if token.count == 0
+
+      atom, new_string = read_token(
+        ignore_separators(string.rest), env, token: token, code: code)
+      code << atom
+
+      [new_string, Str.create("")]
+    end
+
+    def ignore_separators(string)
+      return StrEmpty.instance if fn.empty? string
+      return string if !SEPARATOR.match? String(string.first)
+
+      ignore_separators(string.rest)
+    end
+
+    def read_number(string, env, token: Str.create(""), code: [])
+      number, new_string = *@number.invoke(string)
+      code << number
+
+      [ignore_separators(new_string), Str.create("")]
+    end
+
+    def read_token(string, env, token: Str.create(""), code: [])
+      # 1) token is a unary op function? process. Function is reader?
+      # 2) token anything else, check if next token is a binary op
+      #     if so, also read one more token and process
+      # 3) else, return the symbolized token
+      [Symbol.new(token), string]
     end
   end
 end
