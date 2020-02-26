c74c8b0b4764c6ca8a7e195038e73a5088d23caa
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2020-02-26 18:18:38 +0100

    Let’s go again.

--- lib/feeble/printer/printable.rb
+++
@@ -1,7 +0,0 @@
-module Feeble::Printer
-  module Printable
-    def to_print
-      to_s
-    end
-  end
-end
--- spec/feeble/runtime/argument_type_mismatch.rb
+++
@@ -1,7 +0,0 @@
-module Feeble::Runtime
-  class ArgumentTypeMismatch < StandardError
-    def initialize(given_type, expected_type)
-      super "< #{expected_type.class} > received while expecting < #{expected_type} >"
-    end
-  end
-end
